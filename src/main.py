16be6d79a32ba38c61dc24c9d8a95a4c07384f1b
Chase Roohms <chaseroohms@gmail.com>
2026-02-06 20:23:46 -0600

    feat: implement argument parsing for schema validation inputs

--- src/main.py
+++ src/main.py
@@ -1,1 +1,21 @@
-print("Hello World!")
+import argparse
+from pprint import pp
+
+def main():
+    parser = argparse.ArgumentParser(
+        description="Schema Validator",
+        formatter_class=lambda prog: argparse.HelpFormatter(
+            prog, max_help_position=40, width=120)
+    )
+    parser.add_argument("--files", type=str, help="files to validate")
+    parser.add_argument("--file-format", type=str, help="file format")
+    parser.add_argument("--schema-file", type=str, help="schema file")
+    parser.add_argument("--schema-url", type=str, help="schema URL")
+    parser.add_argument("--schema-format", type=str, help="schema format")
+    parser.add_argument("--output-format", type=str, help="output format")
+    parser.add_argument("--output-file", type=str, help="output file")
+    args = parser.parse_args()
+    pp(vars(args))
+
+if __name__ == "__main__":
+    main()
